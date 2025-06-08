cadd279e317a044daf12a462244ca39f4696ea51
Yiğit Hakverdi <hakverdiyigit@gmail.com>
2025-06-09 00:25:01 +0300

    Updated port init

--- include/port.h
+++ include/port.h
@@ -8,5 +8,11 @@ int port_init(uint16_t port, struct rte_mempool *mbuf_pool);
 void display_mac_address(uint16_t port_id);
 void check_ports_available();
 void setup_port(uint16_t port_id, struct rte_mempool *mbuf_pool, int is_rx);
+int get_and_configure_dev_info(uint16_t port, struct rte_eth_conf *port_conf, struct rte_eth_dev_info *dev_info);
+int setup_rx_queues(uint16_t port, uint16_t rx_rings, uint16_t nb_rxd, struct rte_mempool *mbuf_pool);
+int setup_tx_queues(uint16_t port, uint16_t tx_rings, uint16_t nb_txd, struct rte_eth_dev_info *dev_info, struct rte_eth_conf *port_conf);
+int start_port(uint16_t port);
+int display_port_mac(uint16_t port);
+int enable_promiscuous(uint16_t port);
 
 #endif  // PORT_H
--- src/port.c
+++ src/port.c
@@ -1,69 +1,111 @@
 #include "port.h"
+
 #include "common.h"
 #include "latency.h"
 
+/*
+ * port_init - Initialize a DPDK Ethernet port for RX and TX operation.
+ *
+ * This function configures a given Ethernet port for packet I/O using DPDK. It performs
+ * the following steps:
+ *   1. Validates the port ID.
+ *   2. Retrieves and configures device information and offloads.
+ *   3. Configures the Ethernet device.
+ *   4. Sets up RX and TX queues.
+ *   5. Starts the Ethernet device.
+ *   6. Displays the port's MAC address.
+ *   7. Enables promiscuous mode.
+ *
+ * The function is refactored for clarity and reusability, with each step handled by a
+ * dedicated static helper function. Returns 0 on success, or a negative error code on failure.
+ */
 int port_init(uint16_t port, struct rte_mempool *mbuf_pool) {
   struct rte_eth_conf port_conf;
   const uint16_t rx_rings = 1, tx_rings = 1;
   uint16_t nb_rxd = RX_RING_SIZE;
   uint16_t nb_txd = TX_RING_SIZE;
-  int retval;
-  uint16_t q;
   struct rte_eth_dev_info dev_info;
-  struct rte_eth_txconf txconf;
+  int retval;
 
   if (!rte_eth_dev_is_valid_port(port)) return -1;
-
   memset(&port_conf, 0, sizeof(struct rte_eth_conf));
 
-  retval = rte_eth_dev_info_get(port, &dev_info);
-  if (retval != 0) {
-    printf("Error during getting device (port %u) info: %s\n", port, strerror(-retval));
-    return retval;
-  }
+  retval = get_and_configure_dev_info(port, &port_conf, &dev_info);
+  if (retval != 0) return retval;
 
-  if (dev_info.tx_offload_capa & RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE)
-    port_conf.txmode.offloads |= RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE;
-
-  /* Configure the Ethernet device. */
   retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
   if (retval != 0) return retval;
 
   retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
   if (retval != 0) return retval;
 
-  /* Allocate and set up 1 RX queue per Ethernet port. */
-  for (q = 0; q < rx_rings; q++) {
-    retval = rte_eth_rx_queue_setup(port, q, nb_rxd, rte_eth_dev_socket_id(port), NULL, mbuf_pool);
-    if (retval < 0) return retval;
-  }
-
-  txconf = dev_info.default_txconf;
-  txconf.offloads = port_conf.txmode.offloads;
-  /* Allocate and set up 1 TX queue per Ethernet port. */
-  for (q = 0; q < tx_rings; q++) {
-    retval = rte_eth_tx_queue_setup(port, q, nb_txd, rte_eth_dev_socket_id(port), &txconf);
-    if (retval < 0) return retval;
-  }
-
-  /* Starting Ethernet port. 8< */
-  retval = rte_eth_dev_start(port);
-  /* >8 End of starting of ethernet port. */
-  if (retval < 0) return retval;
-
-  /* Display the port MAC address. */
-  struct rte_ether_addr addr;
-  retval = rte_eth_macaddr_get(port, &addr);
+  retval = setup_rx_queues(port, rx_rings, nb_rxd, mbuf_pool);
   if (retval != 0) return retval;
 
+  retval = setup_tx_queues(port, tx_rings, nb_txd, &dev_info, &port_conf);
+  if (retval != 0) return retval;
+
+  retval = start_port(port);
+  if (retval != 0) return retval;
+
+  retval = display_port_mac(port);
+  if (retval != 0) return retval;
+
+  retval = enable_promiscuous(port);
+  if (retval != 0) return retval;
+
+  return 0;
+}
+
+static int get_and_configure_dev_info(uint16_t port, struct rte_eth_conf *port_conf,
+                                      struct rte_eth_dev_info *dev_info) {
+  int retval = rte_eth_dev_info_get(port, dev_info);
+  if (retval != 0) {
+    printf("Error during getting device (port %u) info: %s\n", port, strerror(-retval));
+    return retval;
+  }
+  if (dev_info->tx_offload_capa & RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE)
+    port_conf->txmode.offloads |= RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE;
+  return 0;
+}
+
+static int setup_rx_queues(uint16_t port, uint16_t rx_rings, uint16_t nb_rxd, struct rte_mempool *mbuf_pool) {
+  for (uint16_t q = 0; q < rx_rings; q++) {
+    int retval = rte_eth_rx_queue_setup(port, q, nb_rxd, rte_eth_dev_socket_id(port), NULL, mbuf_pool);
+    if (retval < 0) return retval;
+  }
+  return 0;
+}
+
+static int setup_tx_queues(uint16_t port, uint16_t tx_rings, uint16_t nb_txd,
+                           struct rte_eth_dev_info *dev_info, struct rte_eth_conf *port_conf) {
+  struct rte_eth_txconf txconf = dev_info->default_txconf;
+  txconf.offloads = port_conf->txmode.offloads;
+  for (uint16_t q = 0; q < tx_rings; q++) {
+    int retval = rte_eth_tx_queue_setup(port, q, nb_txd, rte_eth_dev_socket_id(port), &txconf);
+    if (retval < 0) return retval;
+  }
+  return 0;
+}
+
+static int start_port(uint16_t port) {
+  int retval = rte_eth_dev_start(port);
+  if (retval < 0) return retval;
+  return 0;
+}
+
+static int display_port_mac(uint16_t port) {
+  struct rte_ether_addr addr;
+  int retval = rte_eth_macaddr_get(port, &addr);
+  if (retval != 0) return retval;
   printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n", port,
          RTE_ETHER_ADDR_BYTES(&addr));
+  return 0;
+}
 
-  /* Enable RX in promiscuous mode for the Ethernet device. */
-  retval = rte_eth_promiscuous_enable(port);
-  /* End of setting RX port in promiscuous mode. */
+static int enable_promiscuous(uint16_t port) {
+  int retval = rte_eth_promiscuous_enable(port);
   if (retval != 0) return retval;
-
   return 0;
 }
 
