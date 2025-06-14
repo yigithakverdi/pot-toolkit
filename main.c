2f342433cc0cad3482cd0b9874b125b9b55118cc
yigithakverdi <hakverdiyigit@gmail.com>
2025-06-14 15:01:01 +0000

    Update on next_hop for ARP table

--- main.c
+++ main.c
@@ -7,8 +7,7 @@
 
 int main(int argc, char *argv[]) {
   printf("Initializing next-hop table at startup\n");
-  add_next_hop("2001:db8:0:1::2", "02:0c:b4:7a:8c:6d");
-  add_next_hop("2001:db8:0:2::2", "02:0c:b4:7a:8c:6e");
+  add_next_hop("2600:1f18:abcd:1234::1", "02:f5:27:51:bc:1d");
 
   const char *role = "ingress";
 
@@ -49,10 +48,8 @@ int main(int argc, char *argv[]) {
            mac_addr.addr_bytes[0], mac_addr.addr_bytes[1], mac_addr.addr_bytes[2],
            mac_addr.addr_bytes[3], mac_addr.addr_bytes[4], mac_addr.addr_bytes[5]);
     // Print known IPs if available
-    if (port_id_iter < sizeof(port_ipv4s)/sizeof(port_ipv4s[0]))
-      printf("  IPv4: %s\n", port_ipv4s[port_id_iter]);
-    if (port_id_iter < sizeof(port_ipv6s)/sizeof(port_ipv6s[0]))
-      printf("  IPv6: %s\n", port_ipv6s[port_id_iter]);
+    printf("  IPv6: %s\n", port_ipv6s[port_id_iter]);    
+    printf("  IPv4: %s\n", port_ipv4s[port_id_iter]);
     printf("  Link status: %s, Speed: %u Mbps, Duplex: %s\n",
            link.link_status ? "UP" : "DOWN",
            link.link_speed,
