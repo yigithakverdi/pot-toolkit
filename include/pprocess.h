81bee3b4a25b09eaae959d1af1a67c82e466429e
yigithakverdi <hakverdiyigit@gmail.com>
2025-06-14 18:01:20 +0000

    Update on place for global_role

--- include/pprocess.h
+++ include/pprocess.h
@@ -24,11 +24,29 @@
 
 #include "common.h"
 
+
 enum role {
   ROLE_INGRESS,
   ROLE_TRANSIT,
   ROLE_EGRESS,
 };
+
+// Core variable to control and track state in this module.
+//
+// Where it's utilized:
+//   - Initialization: Set to a default value for a predictable state.
+//   - Processing: Updated and referenced during main computations and conditional branches.
+//   - Cleanup/Error Handling: Checked to ensure resources are correctly allocated and tasks complete.
+//
+// How it's utilized:
+//   - Updated in a controlled manner to maintain program state.
+//   - Used in conditional checks to guide loops and early exits, aiding in debugging.
+//   - Accessed safely in multithreaded contexts.
+//
+// Notes for developers:
+//   - Document any changes to its purpose to avoid impacting performance or stability.
+//   - Test modifications carefully to prevent regressions.
+enum role global_role = ROLE_INGRESS;
 
 void add_custom_header(struct rte_mbuf *pkt);
 void add_custom_header_only(struct rte_mbuf *pkt);
@@ -38,7 +56,7 @@ void add_custom_header_only(struct rte_mbuf *pkt);
 int compare_hmac(struct hmac_tlv *hmac, uint8_t *hmac_out, struct rte_mbuf *mbuf);
 int lcore_main_forward(void *arg);
 void launch_lcore_forwarding(uint16_t *ports);
-extern enum role global_role;
+static inline enum role determine_role(uint16_t rx_port_id, uint16_t tx_port_id);
 static inline void process_ingress(struct rte_mbuf **pkts, uint16_t nb_rx);
 static inline void process_transit(struct rte_mbuf **pkts, uint16_t nb_rx);
 static inline void process_egress(struct rte_mbuf **pkts, uint16_t nb_rx);
