edb326712d6fcfdfd560eb03ee8b77b2d4b52876
yigithakverdi <hakverdiyigit@gmail.com>
2025-06-19 17:15:13 +0000

    Update on hop and SRH

--- main.c
+++ main.c
@@ -8,8 +8,9 @@
 int main(int argc, char *argv[]) {
   printf("Initializing next-hop table at startup\n");
   // add_next_hop("2600:1f18:abcd:1234::1", "02:f5:27:51:bc:1d");
-  add_next_hop("2a05:d014:dc7:12dc:9648:6bf3:e182:c7b4", "02:f5:27:51:bc:1d");
   add_next_hop("2a05:d014:dc7:1209:8169:d7d9:3bcb:d2b3", "02:5f:68:c7:cc:cd");
+  add_next_hop("2a05:d014:dc7:12dc:9648:6bf3:e182:c7b4", "02:f5:27:51:bc:1d");
+  
   
 
   // Find "--" to locate app-specific args
