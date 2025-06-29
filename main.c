d071469e4732d958a9e4f25307d219743b615880
Yigit <hakverdiyigit@gmail.com>
2025-06-29 16:43:12 +0300

    Removed some of the uncessary comments

--- main.c
+++ main.c
@@ -11,13 +11,11 @@
 
 int main(int argc, char *argv[]) {
   printf("Initializing next-hop table at startup\n");
-  // add_next_hop("2600:1f18:abcd:1234::1", "02:f5:27:51:bc:1d"); 
   add_next_hop("2a05:d014:dc7:1209:8169:d7d9:3bcb:d2b3", "02:5f:68:c7:cc:cd");
   add_next_hop("2a05:d014:dc7:12dc:9648:6bf3:e182:c7b4", "02:f5:27:51:bc:1d");
   
   init_logging("/var/log/dpdk-pot", "app", RTE_LOG_INFO);
-
-  // Find "--" to locate app-specific args
+  
   int app_arg_start = 1;
   for (int i = 1; i < argc; ++i) {
     if (strcmp(argv[i], "--") == 0) {
@@ -26,7 +24,6 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // Parse app-specific args
   for (int i = app_arg_start; i < argc; ++i) {
     if (strcmp(argv[i], "--role") == 0 && i + 1 < argc) {
       if (strcmp(argv[i + 1], "ingress") == 0)
@@ -35,7 +32,7 @@ int main(int argc, char *argv[]) {
         global_role = ROLE_TRANSIT;
       else if (strcmp(argv[i + 1], "egress") == 0)
         global_role = ROLE_EGRESS;
-      i++;  // skip value
+      i++;  
     }
   }
 
@@ -46,13 +43,10 @@ int main(int argc, char *argv[]) {
   register_tsc_dynfield();
 
   uint16_t port_id = 0;
-  // uint16_t tx_port_id = 1;
 
   setup_port(port_id, mbuf_pool, 1);
-  // setup_port(tx_port_id, mbuf_pool, 0);  // TX
   printf("TSC frequency: %" PRIu64 " Hz\n", rte_get_tsc_hz());
 
-  // Print DPDK port info after EAL and before main logic
   uint16_t nb_ports = rte_eth_dev_count_avail();
   printf(
       "\n==== DPDK Port Information ===="
@@ -73,7 +67,6 @@ int main(int argc, char *argv[]) {
     printf("  Driver: %s\n", dev_info.driver_name ? dev_info.driver_name : "N/A");
     printf("  MAC: %02X:%02X:%02X:%02X:%02X:%02X\n", mac_addr.addr_bytes[0], mac_addr.addr_bytes[1],
            mac_addr.addr_bytes[2], mac_addr.addr_bytes[3], mac_addr.addr_bytes[4], mac_addr.addr_bytes[5]);
-    // Print known IPs if available
     printf("  Link status: %s, Speed: %u Mbps, Duplex: %s\n", link.link_status ? "UP" : "DOWN",
            link.link_speed, link.link_duplex == RTE_ETH_LINK_FULL_DUPLEX ? "full" : "half");
   }
@@ -81,7 +74,6 @@ int main(int argc, char *argv[]) {
       "==== End DPDK Port Information ===="
       "\n\n");
 
-  // uint16_t ports[2] = {port_id, tx_port_id};
   uint16_t ports[1] = {port_id};
   printf("Starting %u role on port %u\n", global_role, port_id);
   launch_lcore_forwarding(ports);
