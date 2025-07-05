f896dd2378ffa5c9dbaeaa8595a48c463cb812eb
Yigit <hakverdiyigit@gmail.com>
2025-07-05 20:40:29 +0300

    Adding basic port initi functions

---
+++ src/port.c
@@ -0,0 +1,15 @@
+#include "port.h"
+
+
+#include <rte_mempool.h>
+#include <stdint.h>
+
+int port_init(uint16_t port, struct rte_mempool *mbuf_pool) {
+
+    return 0;
+}
+
+void check_ports() {
+
+}
+
