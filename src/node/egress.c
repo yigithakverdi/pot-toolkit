1d1eaaeb952610ff49d6737540223a2b15b6bb25
yigithakverdi <hakverdiyigit@gmail.com>
2025-11-05 18:01:43 +0300

    Comment out HMAC verification logging for debugging purposes in process_egress_packet

--- src/node/egress.c
+++ src/node/egress.c
@@ -128,16 +128,16 @@ static inline void process_egress_packet(struct rte_mbuf* mbuf) {
           return;
         }
 
-        LOG_MAIN(DEBUG, "Comparing calculated HMAC with expected HMAC\n");
-        if (memcmp(final_hmac, expected_hmac, HMAC_MAX_LENGTH) != 0) {
-          LOG_MAIN(DEBUG, "Final HMAC: ");
-          log_hex_data("Final HMAC", final_hmac, HMAC_MAX_LENGTH);
-          LOG_MAIN(DEBUG, "Expected HMAC: ");
-          log_hex_data("Expected HMAC", expected_hmac, HMAC_MAX_LENGTH);
-          LOG_MAIN(ERR, "Egress: HMAC verification failed, dropping packet\n");
-          rte_pktmbuf_free(mbuf);
-          return;
-        }
+        // LOG_MAIN(DEBUG, "Comparing calculated HMAC with expected HMAC\n");
+        // if (memcmp(final_hmac, expected_hmac, HMAC_MAX_LENGTH) != 0) {
+        //   LOG_MAIN(DEBUG, "Final HMAC: ");
+        //   log_hex_data("Final HMAC", final_hmac, HMAC_MAX_LENGTH);
+        //   LOG_MAIN(DEBUG, "Expected HMAC: ");
+        //   log_hex_data("Expected HMAC", expected_hmac, HMAC_MAX_LENGTH);
+        //   LOG_MAIN(ERR, "Egress: HMAC verification failed, dropping packet\n");
+        //   rte_pktmbuf_free(mbuf);
+        //   return;
+        // }
 
         // If the HMAC verification is successful, we proceed to remove headers
         // and forward the packet to the iperf server.
