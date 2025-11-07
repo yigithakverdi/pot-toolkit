5c5aa4219a0adfc39fc5e840030e43e65a89e245
yigithakverdi <hakverdiyigit@gmail.com>
2025-11-07 21:53:17 +0300

    Reduce NONCE_LENGTH from 16 to 8 in crypto.h and headers.h for consistency and memory optimization

--- include/headers.h
+++ include/headers.h
@@ -75,7 +75,7 @@ struct pot_tlv {
   uint8_t reserved;           // Reserved field (1 byte)
   uint8_t nonce_length;       // Nonce Length field (1 byte)
   uint32_t key_set_id;        // Key Set ID (4 bytes)
-  uint8_t nonce[16];          // Nonce (variable length)
+  uint8_t nonce[8];          // Nonce (variable length)
   uint8_t encrypted_hmac[32]; // Encrypted HMAC (variable length)
 };
 
